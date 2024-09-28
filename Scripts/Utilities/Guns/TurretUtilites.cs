019bb2e5fd83f18bda87f816bf103a63f55a6d1f
bernardzaborniak <benito4747@live.com>
2024-09-28 22:14:39 +0200

    added more turret utility methods

--- Scripts/Utilities/Guns/TurretUtilites.cs
+++ Scripts/Utilities/Guns/TurretUtilites.cs
@@ -18,31 +18,60 @@ namespace Benito.ScriptingFoundations.Utilities
             Transform turretAnchorTransform, Transform gunAnchorTransform, TurretSettings settings, float deltaTime)
         {
             Vector3 targetDirectionInTurretSpace = Quaternion.Inverse(turretAnchorTransform.rotation) * (targetPos - turretAnchorTransform.position);
-            float targetTurretAngles = Vector2.SignedAngle(targetDirectionInTurretSpace.ToVector2_xz(), Vector2.up);
+            float targetTurretRot = Vector2.SignedAngle(targetDirectionInTurretSpace.ToVector2_xz(), Vector2.up);
             Vector3 targetDirectionInGunSpace = Quaternion.Inverse(gunAnchorTransform.rotation) * (targetPos - gunAnchorTransform.position);
             float xzLength = targetDirectionInGunSpace.ToVector2_xz().magnitude;
             Vector2 targetGunSpaceDirectionFlattened = new Vector2(xzLength, targetDirectionInGunSpace.y);
-            float targetGunAngles = Vector2.SignedAngle(targetGunSpaceDirectionFlattened.normalized, Vector2.right);
+            float targetGunRot = Vector2.SignedAngle(targetGunSpaceDirectionFlattened.normalized, Vector2.right);
 
             // allow to go over back with turret
             if (!settings.hasTurretRotLimit)
             {
-                if (state.turretRotation < 0 && targetTurretAngles > 0 && targetTurretAngles - state.turretRotation > 180)
+                if (state.turretRotation < 0 && targetTurretRot > 0 && targetTurretRot - state.turretRotation > 180)
                 {
-                    targetTurretAngles = targetTurretAngles - 360;
+                    targetTurretRot = targetTurretRot - 360;
                 }
-                else if(state.turretRotation > 0 && targetTurretAngles < 0 && targetTurretAngles - state.turretRotation < -180)
+                else if(state.turretRotation > 0 && targetTurretRot < 0 && targetTurretRot - state.turretRotation < -180)
                 {
-                    targetTurretAngles = targetTurretAngles + 360;
+                    targetTurretRot = targetTurretRot + 360;
                 }
             }
                
 
-            state.turretRotation = FloatUtilities.BlendLinearly(state.turretRotation, targetTurretAngles, settings.turretRotationSpeed, deltaTime);
-            state.gunRotation = FloatUtilities.BlendLinearly(state.gunRotation, targetGunAngles, settings.gunRotationSpeed, deltaTime);
+            state.turretRotation = FloatUtilities.BlendLinearly(state.turretRotation, targetTurretRot, settings.turretRotationSpeed, deltaTime);
+            state.gunRotation = FloatUtilities.BlendLinearly(state.gunRotation, targetGunRot, settings.gunRotationSpeed, deltaTime);
 
 
             if(settings.hasTurretRotLimit)
+                state.turretRotation = Mathf.Clamp(state.turretRotation, settings.turretRotLeftLimit, settings.turretRotRightLimit);
+
+            if (settings.hasGunRotLimit)
+                state.gunRotation = Mathf.Clamp(state.gunRotation, settings.gunRotLowerLimit, settings.gunRotUpperLimit);
+        }
+
+        public static void CalculateStateAnglesLinearSpeed(ref TurretState state, float targetTurretRot, float targetGunRot,
+            Transform turretAnchorTransform, Transform gunAnchorTransform, TurretSettings settings, float deltaTime)
+        {
+
+            // allow to go over back with turret
+            if (!settings.hasTurretRotLimit)
+            {
+                if (state.turretRotation < 0 && targetTurretRot > 0 && targetTurretRot - state.turretRotation > 180)
+                {
+                    targetTurretRot = targetTurretRot - 360;
+                }
+                else if (state.turretRotation > 0 && targetTurretRot < 0 && targetTurretRot - state.turretRotation < -180)
+                {
+                    targetTurretRot = targetTurretRot + 360;
+                }
+            }
+
+
+            state.turretRotation = FloatUtilities.BlendLinearly(state.turretRotation, targetTurretRot, settings.turretRotationSpeed, deltaTime);
+            state.gunRotation = FloatUtilities.BlendLinearly(state.gunRotation, targetGunRot, settings.gunRotationSpeed, deltaTime);
+
+
+            if (settings.hasTurretRotLimit)
                 state.turretRotation = Mathf.Clamp(state.turretRotation, settings.turretRotLeftLimit, settings.turretRotRightLimit);
 
             if (settings.hasGunRotLimit)
@@ -53,28 +82,55 @@ namespace Benito.ScriptingFoundations.Utilities
             Transform turretAnchorTransform, Transform gunAnchorTransform, TurretSettings settings, float deltaTime)
         {
             Vector3 targetDirectionInTurretSpace = Quaternion.Inverse(turretAnchorTransform.rotation) * (targetPos - turretAnchorTransform.position);
-            float targetTurretAngles = Vector2.SignedAngle(targetDirectionInTurretSpace.ToVector2_xz(), Vector2.up);
+            float targetTurretRot = Vector2.SignedAngle(targetDirectionInTurretSpace.ToVector2_xz(), Vector2.up);
             Vector3 targetDirectionInGunSpace = Quaternion.Inverse(gunAnchorTransform.rotation) * (targetPos - gunAnchorTransform.position);
             float xzLength = targetDirectionInGunSpace.ToVector2_xz().magnitude;
             Vector2 targetGunSpaceDirectionFlattened = new Vector2(xzLength, targetDirectionInGunSpace.y);
-            float targetGunAngles = Vector2.SignedAngle(targetGunSpaceDirectionFlattened.normalized, Vector2.right);
+            float targetGunRot = Vector2.SignedAngle(targetGunSpaceDirectionFlattened.normalized, Vector2.right);
 
             // allow to go over back with turret
             if (!settings.hasTurretRotLimit)
             {
-                if (state.turretRotation < 0 && targetTurretAngles > 0 && targetTurretAngles - state.turretRotation > 180)
+                if (state.turretRotation < 0 && targetTurretRot > 0 && targetTurretRot - state.turretRotation > 180)
                 {
-                    targetTurretAngles = targetTurretAngles - 360;
+                    targetTurretRot = targetTurretRot - 360;
                 }
-                else if (state.turretRotation > 0 && targetTurretAngles < 0 && targetTurretAngles - state.turretRotation < -180)
+                else if (state.turretRotation > 0 && targetTurretRot < 0 && targetTurretRot - state.turretRotation < -180)
                 {
-                    targetTurretAngles = targetTurretAngles + 360;
+                    targetTurretRot = targetTurretRot + 360;
                 }
             }
 
-            state.turretRotation = FloatUtilities.BlendWithAccelAndDecel(state.turretRotation, targetTurretAngles, 
+            state.turretRotation = FloatUtilities.BlendWithAccelAndDecel(state.turretRotation, targetTurretRot, 
                 settings.turretRotationSpeed, settings.turretAcceleration,ref state.turretVelocity, true, deltaTime);
-            state.gunRotation = FloatUtilities.BlendWithAccelAndDecel(state.gunRotation, targetGunAngles, 
+            state.gunRotation = FloatUtilities.BlendWithAccelAndDecel(state.gunRotation, targetGunRot, 
+                settings.gunRotationSpeed, settings.gunAcceleration, ref state.gunVelocity, true, deltaTime);
+
+            if (settings.hasTurretRotLimit)
+                state.turretRotation = Mathf.Clamp(state.turretRotation, settings.turretRotLeftLimit, settings.turretRotRightLimit);
+            if (settings.hasGunRotLimit)
+                state.gunRotation = Mathf.Clamp(state.gunRotation, settings.gunRotLowerLimit, settings.gunRotUpperLimit);
+        }
+
+        public static void CalculateStateAnglesAcceleration(ref TurretState state, float targetTurretRot, float targetGunRot,
+            Transform turretAnchorTransform, Transform gunAnchorTransform, TurretSettings settings, float deltaTime)
+        {
+            // allow to go over back with turret
+            if (!settings.hasTurretRotLimit)
+            {
+                if (state.turretRotation < 0 && targetTurretRot > 0 && targetTurretRot - state.turretRotation > 180)
+                {
+                    targetTurretRot = targetTurretRot - 360;
+                }
+                else if (state.turretRotation > 0 && targetTurretRot < 0 && targetTurretRot - state.turretRotation < -180)
+                {
+                    targetTurretRot = targetTurretRot + 360;
+                }
+            }
+
+            state.turretRotation = FloatUtilities.BlendWithAccelAndDecel(state.turretRotation, targetTurretRot,
+                settings.turretRotationSpeed, settings.turretAcceleration, ref state.turretVelocity, true, deltaTime);
+            state.gunRotation = FloatUtilities.BlendWithAccelAndDecel(state.gunRotation, targetGunRot,
                 settings.gunRotationSpeed, settings.gunAcceleration, ref state.gunVelocity, true, deltaTime);
 
             if (settings.hasTurretRotLimit)
