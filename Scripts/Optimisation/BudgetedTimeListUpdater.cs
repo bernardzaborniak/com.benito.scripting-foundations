fc6922472d7dafd6ef651b937644145214fc4c8f
bernardzaborniak <benito4747@live.com>
2025-01-22 21:47:08 +0100

    added namespace to BugetedTimeListUpdater

--- Scripts/Optimisation/BudgetedTimeListUpdater.cs
+++ Scripts/Optimisation/BudgetedTimeListUpdater.cs
@@ -4,71 +4,74 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
-public abstract class BudgetedTimeListUpdater<T> : IBudgetedOperation
+namespace Benito.ScriptingFoundations.Optimisation
 {
-    public bool Finished { get; protected set; }
-
-    public float Progress { get; protected set; }
-
-    // Watch out -  if you switch the budget very low, it first needs to finish one whole iteraion on this low budget,
-    // probably only one item per frame, before starting a new iteration with the new budget
-    public float TimeBudget { get; protected set; }
-
-    protected int stoppedAtIndex;
-    protected List<T> listToTraverse;
-
-    protected Stopwatch stopwatch;
-
-    public BudgetedTimeListUpdater()
+    public abstract class BudgetedTimeListUpdater<T> : IBudgetedOperation
     {
-        Finished = true;
-        stopwatch = new Stopwatch();
-    }
+        public bool Finished { get; protected set; }
 
-    public void Restart(List<T> listToTraverse, float timeBudgetInMs)
-    {
-        this.listToTraverse = listToTraverse;
-        stoppedAtIndex = 0;
-        TimeBudget = timeBudgetInMs;
-        Finished = false;
+        public float Progress { get; protected set; }
 
-    }
+        // Watch out -  if you switch the budget very low, it first needs to finish one whole iteraion on this low budget,
+        // probably only one item per frame, before starting a new iteration with the new budget
+        public float TimeBudget { get; protected set; }
 
-    public void Update(float deltaTime)
-    {
-        stopwatch.Restart();
+        protected int stoppedAtIndex;
+        protected List<T> listToTraverse;
 
-        for (int i = stoppedAtIndex; i < listToTraverse.Count; i++)
+        protected Stopwatch stopwatch;
+
+        public BudgetedTimeListUpdater()
         {
-            PerFrameOperation(i);
-
-            if (GetElapsedMiliseconds()>TimeBudget)
-            {
-                StopIterationForThisFrame(i);
-                return;
-            }
+            Finished = true;
+            stopwatch = new Stopwatch();
         }
 
-        stopwatch.Stop();
-        Finished = true;
-    }
+        public void Restart(List<T> listToTraverse, float timeBudgetInMs)
+        {
+            this.listToTraverse = listToTraverse;
+            stoppedAtIndex = 0;
+            TimeBudget = timeBudgetInMs;
+            Finished = false;
 
-    protected abstract void PerFrameOperation(int index);
+        }
 
-    protected void StopIterationForThisFrame(int index)
-    {
-        stopwatch.Stop();
-        stoppedAtIndex = index + 1;
-        Progress = (1f * index) / (1f * listToTraverse.Count);
-    }
+        public void Update(float deltaTime)
+        {
+            stopwatch.Restart();
 
-    /// <summary>
-    /// Dont use often, calling this method means its being calculated twice
-    /// </summary>
-    /// <returns></returns>
-    protected double GetElapsedMiliseconds()
-    {
-        return (stopwatch.ElapsedTicks * 1000.0) / Stopwatch.Frequency;
+            for (int i = stoppedAtIndex; i < listToTraverse.Count; i++)
+            {
+                PerFrameOperation(i);
+
+                if (GetElapsedMiliseconds() > TimeBudget)
+                {
+                    StopIterationForThisFrame(i);
+                    return;
+                }
+            }
+
+            stopwatch.Stop();
+            Finished = true;
+        }
+
+        protected abstract void PerFrameOperation(int index);
+
+        protected void StopIterationForThisFrame(int index)
+        {
+            stopwatch.Stop();
+            stoppedAtIndex = index + 1;
+            Progress = (1f * index) / (1f * listToTraverse.Count);
+        }
+
+        /// <summary>
+        /// Dont use often, calling this method means its being calculated twice
+        /// </summary>
+        /// <returns></returns>
+        protected double GetElapsedMiliseconds()
+        {
+            return (stopwatch.ElapsedTicks * 1000.0) / Stopwatch.Frequency;
+        }
     }
 }
 
