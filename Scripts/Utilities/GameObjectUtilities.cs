bb3b69a1e707f0e0bbc39c9f1a392f20dab7dca2
Bernard <22891809+bernardzaborniak@users.noreply.github.com>
2025-12-13 16:30:33 +0100

    added global coroutine host

--- Scripts/Utilities/GameObjectUtilities.cs
+++ Scripts/Utilities/GameObjectUtilities.cs
@@ -5,6 +5,13 @@ namespace Benito.ScriptingFoundations.Utilities
 {
     public static class GameObjectUtilities
     {
+        /// <summary>
+        ///  Makes sure that the spawned object has the required component and returns it, if the component is not there, the instantiated Prefab gets destroyed again.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="prefab"></param>
+        /// <param name="spawnParent"></param>
+        /// <returns></returns>
         public static T SpawnPrefabWithComponent<T>(GameObject prefab, Transform spawnParent)
         {
             GameObject spawnedObject = GameObject.Instantiate(prefab, spawnParent);
---
+++ Scripts/Utilities/GlobalCoroutineHost.cs
@@ -0,0 +1,21 @@
+using Benito.ScriptingFoundations.Managers;
+using UnityEngine;
+
+namespace Benito.ScriptingFoundations.Utilities
+{
+    /// <summary>
+    /// Use this empty Monobehaviour as host for your Coroutines, if you can't use your own GameObject.
+    /// </summary>
+    public class GlobalCoroutineHost : SingletonManagerGlobal
+    {
+        public override void InitialiseManager()
+        {
+           
+        }
+
+        public override void UpdateManager()
+        {
+            
+        }
+    }
+}
