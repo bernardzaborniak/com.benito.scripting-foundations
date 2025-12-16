5edd1ee2cd4eca91499cb0a571075861072937ca
Bernard <22891809+bernardzaborniak@users.noreply.github.com>
2025-12-16 18:02:33 +0100

    added global fade host

---
+++ Scripts/Fades/BFadeGlobalHost.cs
@@ -0,0 +1,18 @@
+using Benito.ScriptingFoundations.Managers;
+using UnityEngine;
+
+namespace Benito.ScriptingFoundations.Fades
+{
+    public class BFadeGlobalHost : SingletonManagerGlobal
+    {
+        public override void InitialiseManager()
+        {
+
+        }
+
+        public override void UpdateManager()
+        {
+
+        }
+    }
+}
