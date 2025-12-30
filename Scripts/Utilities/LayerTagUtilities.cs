0c6bf172998dbab72bce7356ef5a374f28f3df91
Bernard <22891809+bernardzaborniak@users.noreply.github.com>
2025-12-30 22:16:01 +0100

    added layer tag utilities

---
+++ Scripts/Utilities/LayerTagUtilities.cs
@@ -0,0 +1,45 @@
+using Benito.ScriptingFoundations.Managers;
+using System.Collections;
+using UnityEngine;
+
+namespace Benito.ScriptingFoundations.Utilities
+{
+    public class LayerTagUtilities
+    {
+        public static void SetLayerRecursively(Transform parent, int layer)
+        {
+            parent.gameObject.layer = layer;
+
+            foreach (Transform child in parent)
+            {
+                SetLayerRecursively(child, layer);
+            }
+        }
+
+        public static void SetTagRecursively(Transform parent, string tag) 
+        {
+            parent.gameObject.tag = tag;
+
+            foreach (Transform child in parent)
+            {
+                SetTagRecursively(child, tag);
+            }
+        }
+
+        /// <summary>
+        /// Might be usefull to change some asynchronous errors, like for example cinemachine deoccluder
+        /// </summary>
+        public static void OverrideTagForSetTime(Transform parent, string tag, float time = 1f)
+        {
+            GlobalManagers.Get<GlobalCoroutineHost>().StartCoroutine(OverrideTagForSetTimeCoroutine(parent,tag,time));
+        }
+
+        static IEnumerator OverrideTagForSetTimeCoroutine(Transform parent, string tag, float time = 1f)
+        {
+            string originalTag = parent.tag;
+            SetTagRecursively(parent,tag);
+            yield return new WaitForSeconds(time);
+            SetTagRecursively(parent,originalTag);
+        }
+    }
+}
